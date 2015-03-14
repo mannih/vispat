d057747d905d054fb72176ba1fc3aab901255f46
Manni Heumann <github@lxxi.org>
2015-03-14 18:02:06 +0100

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # vispat
+
 Vim Server In A Tmux Pane
+
+## Why?
+
+Because vsiatp (Vim Server In A Tmux Pane)
+is so hard to pronounce.
+
+If you are using `tmux` and you like to split your windows into panes
+so that, for example, you have a shell running in the left pane and
+a `vim` instance in the right plane, vispat will allow you to open a
+file in the already running `vim` on the right.
+
+![vispat demo](./demo.gif)
+
+## Requirements
+
+- Of course you need `tmux`
+- You need a version of `vim` that was compiled with server support.
+  Depending on your environment, you will have to find the correct
+  vim-package to install. E.g., on Fedora you need to install `vim-X11`.
+- To be able to load Perl modules by their package name, install
+  `Module::Path`
+
+## Installation
+
+    cd somedir
+    git clone https://github.com/mannih/vispat.git
+
+And in your .bashrc, add the line
+
+    source somedir/vispat/vispat.sh
+
+## Usage
+
+vispat gives you a couple of bash functions to use from the command
+line:
+
+- `ws`
+
+Create a vertical split and start `vim` in the right pane.
+
+- `select_vim_pane` (aliased to `vp`)
+
+Activate the `tmux` pane that contains the `vim` instance.
+
+- `v`
+
+Takes one or more file names as arguments and opens those files in the
+vim instance that can be found in some pane of your current tmux window.
+If no running vim instance can be found, a new one will be started.
+
+## Configuration
+
+Configuration is optional, but you might want to read on if you are
+developing in Perl or like to use `vim`'s tab feature.
+
+### Perl module support
+
+    export vispat_for_perl=1
+
+If you want to be able to edit perl modules by package name:
+
+    v IO::All
+
+will the find the file implementing `IO::All` (you will have to install
+`Module::Path` for this to work)
+
+### Perl module support
+
+    export vispat_use_tabs=1
+
+If you prefer using `vim`'s tabs feature. Every file you load with v will then
+open in a new tab.
+
---
+++ vispat.sh
@@ -0,0 +1,75 @@
+# vispat
+
+MYVIM=/usr/bin/vim
+if [ -f /etc/redhat-release ] && [ -x /usr/bin/vimx ]; then
+    MYVIM=/usr/bin/vimx
+fi
+export MYVIM
+
+alias wp=select_vim_pane
+
+# vim_server_name
+# determines the name of the vim server that
+# we should start or talk to. This will produce
+# a string like VIMSERVER.00_01 where 00 is the
+# session name and 01 is the number of the current
+# window.
+function vim_server_name() {
+    local window=$( printf "%02d_%02d" $( tmux display-message -p '#S' ) $( tmux display-message -p '#I' ) )
+    local server="VIMSERVER.$window"
+    echo $server
+}
+
+function v() {
+    local server=$( vim_server_name )
+    local params="--servername $server"
+    local toggle_pane=1
+
+    # vim tabs require a few extra parameters
+    if [[ -n "$vispat_use_tabs" ]]; then
+        if [ -n "$( $MYVIM --serverlist | grep -w $server )" ]; then
+            params="$params --remote-tab-silent"
+        else
+            toggle_pane=
+            if [ $# -gt 1 ]; then
+                params="$params -p"
+            fi
+        fi
+    fi
+
+    # If the filename contains "::", treat it as a perl module
+    # and run mpath to find it.
+    # TODO: Make this work for more than just the first file
+    if [[ -n "$vispat_for_perl" ]] && [[ $1 == *::* ]]; then
+        file=$( mpath "$1" )
+        params="$params $file"
+    else
+        params="$params $@"
+    fi
+
+    $MYVIM $params
+
+    if [ $toggle_pane ]; then
+        select_vim_pane
+    fi
+}
+
+function select_vim_pane() {
+    local targetpane=$( tmux list-panes -F '#{pane_current_command} #P' | grep vim | cut -d' ' -f2 )
+    if [ "x$targetpane" == "x" ]; then
+        echo "could not find a pane with a running vim"
+    else
+        tmux select-pane -t $targetpane
+    fi
+}
+
+function ws() {
+    if [[ $( tmux list-pane | grep -v active ) ]]; then
+        tmux send-keys -t :.+ C-d
+        sleep 1
+    fi
+
+    tmux split-window -hdp 50
+    tmux send-keys -t :.+ v Enter
+}
+
